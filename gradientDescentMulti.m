2eccfd6f0e73f3ca5a50d42d9a77455e90b25508
shajinzheng <shajinzheng1993@gmail.com>
2014-03-13 16:21:31 +0800

    lab

--- gradientDescentMulti.m
+++ gradientDescentMulti.m
@@ -16,16 +16,12 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
+    n = size(X, 2);
+    delta = zeros(n, 1);
+    for j = 1 : n
+        delta(j) = ((X * theta - y) .* X(:, j))' * ones(m, 1);
+    end
+    theta  = theta - alpha / m * delta;
 
     % ============================================================
 
